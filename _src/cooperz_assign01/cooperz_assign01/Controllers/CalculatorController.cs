7ecea5896f02ae726234e5c18c382b767912ec45
CooperZA <zach@raincitysolutions.com>
2020-01-23 20:55:25 -0800

    a02 complete

---
+++ _src/cooperz_assign01/cooperz_assign01/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using cooperz_assign01.Models;
+
+namespace cooperz_assign01.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculatorModel calculator, string myButton)
+        {
+            switch (myButton)
+            {
+                case "Add":
+                    //add
+                    ViewBag.result = calculator.ValueA + calculator.ValueB;
+                    break;
+                case "Subtract":
+                    //sub
+                    ViewBag.result = calculator.ValueA - calculator.ValueB;
+                    break;
+                case "Multiply":
+                    //mult
+                    ViewBag.result = calculator.ValueA * calculator.ValueB;
+                    break;
+                case "Divide":
+                    //div
+                    ViewBag.result = calculator.ValueA / calculator.ValueB;
+                    break;
+            }
+
+            Response.Write("myButton:" + myButton + "<br>");
+            return View(calculator);
+        }
+    }
+}
---
+++ _src/cooperz_assign01/cooperz_assign01/Models/CalculatorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace cooperz_assign01.Models
+{
+    public class CalculatorModel
+    {
+        [Required] public int ValueA { get; set; }
+        [Required] public int ValueB { get; set; } 
+    }
+}
